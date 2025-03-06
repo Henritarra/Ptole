1bdc713ffa0f14faadedb45cb19c91e267018aa9
Henrique Almeida <henritarre@gmail.com>
2025-03-06 22:54:07 +0100

    Experiments with scrolling

--- scriptanimations.js
+++ scriptanimations.js
@@ -3,7 +3,16 @@ const shapeOne = document.querySelector(".shape1");
 const shapeTwo = document.querySelector(".shape2");
 const shapeThree = document.querySelector(".shape3");
 const backgroundClassic = document.querySelector(".classic");
+const classicPosition = backgroundClassic.getBoundingClientRect();
 const classicImage = document.querySelector(".classical-img");
+const classicText = document.querySelector(".classical-text");
+const classicVideo = document.querySelector(".video-classical");
+const worldSection = document.querySelector(".world");
+const worldPosition = worldSection.getBoundingClientRect();
+const worldImage = document.querySelector(".world-music-img");
+const worldText = document.querySelector(".world-music-text");
+const worldVideo = document.querySelector(".video-world");
+
 
 // console.log(window);
 
@@ -15,13 +24,14 @@ document.addEventListener("scroll", function() {
     shapeOne.style.transform = `translateX(${newMargin}px)`
     shapeTwo.style.transform = `translateX(${newMargin}px)`;
     shapeThree.style.clipPath = `polygon(${50 - (0.05 * -newMargin)}% 0%, 100% 0%, 100% 100%, ${50 - (0.05 * -newMargin)}% 100%)`;
-    
+    // SET COLOT FOR CLASSICAL AND WORLD MUSIC TRANSITION
+    const cor = 230;
     
     if (scrollPosition >= 400) {
         let startWhite = 0;
         let transy = (startWhite - 400 + scrollPosition / 2.2);
         let transyPerc = 18 - ((scrollPosition - 400) / 750) * 18;
-        const cor = 230;
+        
         backgroundClassic.style.background = `linear-gradient(to left,rgb(105, 11, 1) 0%, rgb(${Math.min(cor, transy)}, ${Math.min(cor, transy)}, ${Math.min(cor, transy)}) ${transyPerc}%)`;
         shapeThree.style.background = `linear-gradient(to left,rgb(105, 11, 1) 0%, rgb(${Math.min(cor, transy)}, ${Math.min(cor, transy)}, ${Math.min(cor, transy)}) ${transyPerc}%)`;
         shapeOne.style.background = `linear-gradient(to left,rgb(105, 11, 1) 0%, rgb(${Math.min(cor, transy)}, ${Math.min(cor, transy)}, ${Math.min(cor, transy)}) ${transyPerc}%)`;
@@ -35,4 +45,61 @@ document.addEventListener("scroll", function() {
         // classicImage.style.paddingTop = `900px`;
 
     }
+
+    ////////////////NEW TECHNIQUE//////////////////
+    const screenHeight = window.innerHeight;
+    const newScrollPosition = window.scrollY - window.innerHeight;
+
+    ////////////////CLASSIC//////////////////
+    const visibilityClassic = 0.3;
+    const whenToChangeClassic = classicPosition.top + screenHeight * visibilityClassic;
+    ////Image
+    if(newScrollPosition >= whenToChangeClassic) {
+        classicImage.style.transform = `translateY(${Math.min(30, -(whenToChangeClassic - newScrollPosition) * 0.027)}%)`
+    }
+    ////Text and Video
+    if(newScrollPosition >= whenToChangeClassic) {
+        classicText.style.transform = `translateY(${Math.min(20, -(whenToChangeClassic - newScrollPosition) * 0.01)}%)`;
+        classicVideo.style.transform = `translateY(${Math.min(20, -(whenToChangeClassic - newScrollPosition) * 0.02)}%)`;
+    }
+    
+
+    ////////////////WORLD//////////////////
+    const visibilityWorld = 1.5;
+    const whenToChangeWorld = worldPosition.top + screenHeight * visibilityWorld;
+    ///Image
+    if(newScrollPosition >= whenToChangeWorld) {
+        worldImage.style.transform = `translateY(${Math.min(30, -(whenToChangeWorld - newScrollPosition) * 0.027)}%)`
+    }
+    //Text and Video
+    if(newScrollPosition >= whenToChangeWorld) {
+        worldText.style.transform = `translateY(${Math.min(20, -(whenToChangeWorld - newScrollPosition) * 0.01)}%)`;
+        worldVideo.style.transform = `translateY(${Math.min(20, -(whenToChangeWorld - newScrollPosition) * 0.02)}%)`;
+    }
+    
+
+    ////////////BACKGROUND//////////////
+    const visibility = 0.5;
+    const whenToChange = worldPosition.top + screenHeight * visibility;
+    if (newScrollPosition >= whenToChange) {
+        let equation = (newScrollPosition - whenToChange) / 1000;
+        console.log("Equation", equation);
+        // console.log("worldPosition.top", worldPosition.top);
+        const [r, g, b] = changeBackgroundColor ( [cor, cor, cor], [52, 30, 30], equation);
+        worldSection.style.backgroundColor = `rgb(${Math.max(52, r)}, ${Math.max(30, g)}, ${Math.max(30, b)})`;
+        backgroundClassic.style.background = "none";
+        backgroundClassic.style.backgroundColor = `rgb(${Math.max(52, r)}, ${Math.max(30, g)}, ${Math.max(30, b)})`;
+    }
+
 })
+
+function changeBackgroundColor (startColor, endColor, factor) {
+    return startColor.map((c, i) => Math.round(c + factor * (endColor[i] - c)));
+}
+// let visibility = Math.max(0, Math.min(1, (windowHeight - worldPosition.top) / windowHeight));
+
+// function interpolateColor(start, end, factor) {
+//     return start.map((c, i) => Math.round(c + factor * (end[i] - c)));
+// }
+
+// IntersectionObserver
